6f1098a6d76659a47909e53bf44b93ee7c6cc987
Mads D. Pedersen <madsndp@gmail.com>
2016-12-27 22:41:52 +0100

    gg_avg_kills, gg_avg_deaths and gg_avg_assists are now created using a single dictionary champion_avg_stats instead of calling to the Champion.gg API three times

--- app.py
+++ app.py
@@ -59,9 +59,10 @@ def get_teams(region, summoner_name):
             player_dict['avg_deaths'] = champ['stats']['totalDeathsPerSession']
             player_dict['avg_assists'] = champ['stats']['totalAssists']
             break
-        player_dict['gg_avg_kills'] = champion_gg.get_stats()[riot.get_champion_name(champion_id)]['kills']
-        player_dict['gg_avg_deaths'] = champion_gg.get_stats()[riot.get_champion_name(champion_id)]['deaths']
-        player_dict['gg_avg_assists'] = champion_gg.get_stats()[riot.get_champion_name(champion_id)]['assists']
+        champion_avg_stats = champion_gg.get_stats()
+        player_dict['gg_avg_kills'] = champion_avg_stats[riot.get_champion_name(champion_id)]['kills']
+        player_dict['gg_avg_deaths'] = champion_avg_stats[riot.get_champion_name(champion_id)]['deaths']
+        player_dict['gg_avg_assists'] = champion_avg_stats[riot.get_champion_name(champion_id)]['assists']
 
 
         if player['teamId'] == 100:
