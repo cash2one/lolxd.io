220cb3488d15342732b43e3257813e2dd862302d
Mads D. Pedersen <madsndp@gmail.com>
2016-12-27 17:48:30 +0100

    Implemented key features of Champion_API as a class Documented Champion_API Fixed background randomizer

--- Champion_API.py
+++ Champion_API.py
@@ -1,34 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+    lolxd.championgg.py
+    ~~~~~~~~~~
+
+    This module contacts the Champion.gg API.
+    Found at http://api.champion.gg/
+
+    :copyright: (c) 2016 by Mads Damgaard and Carl Bordum Hansen.
+    :license: MIT, see LICENSE for more details.
+"""
+
 import config
 import requests
+import riot
 
-champion_API_KEY = config.champion_API_KEY
-
+API_KEY = config.CHAMPION_API_KEY
 
 def api_call(query):
-    """Return the json object of a call to the API."""
-    url = f'http://api.champion.gg/{query}?api_key={champion_API_KEY}'
+    """Reqeust the json object of a call to the API.
+
+    :rtype dict
+    """
+    url = f'http://api.champion.gg/{query}?api_key={API_KEY}'
     r = requests.get(url)
+    r.raise_for_status()
     return r.json()
 
 
 def get_all_champion_stats():
-    """Return all stats for all champions."""
+    """Query api for all stats for all champions.
+
+    :rtype linked dicts
+    """
     return api_call('stats')
 
 
-def get_item_set(champion_name):
-    """Return a list of the most winning item set for a given champion."""
-    url = f'champion/{champion_name}/items/finished/mostWins'
-    return api_call(url)[0]['items']
+class Summoner:
 
 
-def get_starting_items(champion_name):
-    """Return a list of the most popular starting items for a given champion."""
-    url = f'champion/{champion_name}/items/starters/mostPopular'
-    return api_call(url)[0]['items']
+    def __init__(self, champion_name):
+        """
+            Initialize an instance of *Summoner* with a champion name
+        """
+        self.champion_name = champion_name
 
 
-def get_skill_order(champion_name):
-    """Return a list of the most popular skill order for a given champion."""
-    url = f'champion/{champion_name}/skills/mostPopular'
-    return api_call(url)[0]['order']
+    def get_item_set(self):
+        """
+        Query the API for the most winning item set for the given champion
+
+        :rtype list of strings
+        """
+        url = f'champion/{self.champion_name}/items/finished/mostWins'
+        items = api_call(url)[0]['items']
+        item_name = [riot.get_item_name(item) for item in items]
+        return(item_name)
+
+
+    def get_starting_items(self):
+        """
+        Query the API for the most popular starting items for the given champion
+
+        :rtype list of strings
+        """
+        url = f'champion/{self.champion_name}/items/starters/mostPopular'
+        items = api_call(url)[0]['items']
+        item_names = [riot.get_item_name(item) for item in items]
+        return(item_names)
+
+
+    def get_skill_order(self):
+        """
+        Query the API for the most popular skill order for the given champion
+
+        :rtype list of strings
+        """
+        url = f'champion/{self.champion_name}/skills/mostPopular'
+        return api_call(url)[0]['order']
