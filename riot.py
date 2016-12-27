d28d96402df6d154716e1cc8a49cf2feccb412c1
Carl Bordum <carl@bordum.dk>
2016-12-27 16:03:49 +0100

    Updated documentation.

--- riot.py
+++ riot.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    lolxd.riot
+    ~~~~~~~~~~
+
+    This module contacts the official League of Legends API.
+
+    :copyright: (c) 2016 by Mads Damgaard and Carl Bordum Hansen.
+    :license: MIT, see LICENSE for more details.
+"""
 import requests
 import config
 
 
 API_KEY = config.API_KEY
+
 platforms = {
     'br': 'BR1',
     'eune': 'EUN1',
@@ -19,7 +30,10 @@ platforms = {
 
 
 def base_request(query, region, static=False, **kwargs):
-    """Make a request to the Riot API."""
+    """Make a request to the Riot API.
+
+    :rtype: dict
+    """
     params = {'api_key': API_KEY}
     for key, value in kwargs.items():
         params[key] = value
@@ -31,31 +45,48 @@ def base_request(query, region, static=False, **kwargs):
 
 
 def api_request(region, version, query):
+    """Make a request to the RIOT API at the URL '/api/lol/'.
+
+    :rtype: dict
+    """
     url = f'api/lol/{region}/{version}/{query}'
     return base_request(url, region)
 
 
 def observer_mode_request(region, query):
+    """Make a request to the Riot API at the URL '/observer-mode/rest/'.
+
+    :rtype: dict
+    """
     url = f'observer-mode/rest/{query}'
     return base_request(url, region)
 
 
 def get_summoner_id(region, summoner_name, version='v1.4'):
-    """Return the corresponding summoner ID."""
+    """Return the corresponding summoner ID.
+
+    :rtype: int
+    """
     query = f'summoner/by-name/{summoner_name}'
     r = api_request(region, version, query)
     return r[summoner_name.lower()]['id']
 
 
 def get_ranked_stats(region, summoner_name, version='v1.3'):
-    """Return the ranked stats of summoner_name."""
+    """Return the ranked stats of summoner_name.
+
+    :rtype: dict
+    """
     summoner_id = get_summoner_id(region, summoner_name)
     query = f'stats/by-summoner/{summoner_id}/ranked'
     return api_request(region, version, query)
 
 
 def get_current_game(region, summoner_name):
-    """Return the information on *summoner_name*s current game."""
+    """Return the information on *summoner_name*s current game.
+
+    :rtype: dict
+    """
     summoner_id = get_summoner_id(region, summoner_name)
     platform_id = platforms[region]
     query = f'consumer/getSpectatorGameInfo/{platform_id}/{summoner_id}'
